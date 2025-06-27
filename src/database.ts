8a7588528bc1a95d0a16d6e450e6058fbc9b4ddc
willi-humphreys <Willi.Neto@rhfranquias.com.br>
2025-06-27 15:02:45 -0300

    chore: Production config

--- src/database.ts
+++ src/database.ts
@@ -6,10 +6,10 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const config: Knex.Config = {
-    client: 'sqlite',
-    connection: {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_URL === 'sqlite' ? {
         filename: env.DATABASE_URL,
-    },
+    } : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
